c3258a017d8d1ce7a57a41e063d09579f2d1f8a5
hoangtu <hoangtutdst@gmail.com>
2021-12-12 02:12:20 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var scrollWindow = function () {
+    $(window).scroll(function () {
+        var $w = $(this),
+            st = $w.scrollTop(),
+            navbar = $('.my_navbar'),
+            sd = $('.js-scroll-wrap');
+
+        if (st > 150) {
+            if (!navbar.hasClass('scrolled')) {
+                navbar.addClass('scrolled');
+            }
+        }
+        if (st < 150) {
+            if (navbar.hasClass('scrolled')) {
+                navbar.removeClass('scrolled sleep');
+            }
+        }
+        if (st > 200) {
+            if (!navbar.hasClass('awake')) {
+                navbar.addClass('awake');
+            }
+
+            if (sd.length > 0) {
+                sd.addClass('sleep');
+            }
+        }
+        if (st < 200) {
+            if (navbar.hasClass('awake')) {
+                navbar.removeClass('awake');
+                navbar.addClass('sleep');
+            }
+            if (sd.length > 0) {
+                sd.removeClass('sleep');
+            }
+        }
+    });
+};
+scrollWindow();
+
+$('.awake').css('position', 'fixed');
