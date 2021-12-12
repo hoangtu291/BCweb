5cbb374c97928eedbd333ca03fce37f5e9ad74d5
hoangtu <hoangtutdst@gmail.com>
2021-12-12 15:17:24 +0700

    15:17/12/12

--- js/script.js
+++ js/script.js
@@ -54,3 +54,33 @@ showBanner();
 $('body').css("max-width", $(window).width() + "px");
 $('.banner-section .banner-image .img-carou').css("width", $(window).width() - 300 + "px");
 $('#carou-inner-banner img.img-banner').css("height", ($(window).width() - 300) / 3 + "px");
+
+
+
+
+// ----------------------------
+
+$('#customers-testimonials').owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 0,
+    autoplay: true,
+    dots: false,
+    autoplayTimeout: 4000,
+    smartSpeed: 300,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1170: {
+            items: 3
+        }
+    }
+});
+
+
+$('body').addClass('loaded');
